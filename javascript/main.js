00d2c55004d308ea82d8e39043b3e98d7600bd96
SherzodQosimjonov <gpolokotgmaylcom@gmail.com>
2023-04-16 14:47:59 +0500

    test

---
+++ javascript/main.js
@@ -0,0 +1,50 @@
+// console.log("SherzodQosimjonov");
+const userNomi = "SherzodQosimjonov"; 
+//const o'zgarmas kalit so'z
+let usernomi = "Sherzod"; 
+// let o'zgaradigan kalit so'z
+console.log(userNomi);
+// userNomi = "Samandar";
+// console.log(userNomi);
+
+// Arithmatic operators
+// let son1 = 24;
+// let son2 = 6;
+// console.log(son1 + son2); // = 30
+// console.log(son1 - son2); // = 18
+// console.log(son1 * son2); // = 144
+// console.log(son1 / son2); // = 4
+// console.log(son1 % son2); // = 0
+// % (foiz) belgisi qoldiqli bo'lishni bildiradi 
+// yani sonning qoldig'ini ekranga chiqaradi
+// console.log(++son1); // = 24 // son1++ = son1 + 1
+// (++son1) - post increment
+// (son1++) - increment
+// console.log(son1);
+// console.log(son1);
+// console.log(++son2); // = 7
+
+// console.log(--son1); // 23
+// (--son1) - post decrement
+// console.log(son1--); //23
+// console.log(son1); // 22
+
+//  Comparison operators
+
+let son1 = 24;
+let son2 = "24";
+
+// console.log(son1 > son2); // true
+// console.log(son1 < son2); // false
+// console.log(son1 <= son2); // false
+// console.log(son1 >= son2); // true
+// console.log(son1 == son2); // true
+// == - shunchaki tengmi degan buyruq
+// console.log(son1 === son2); // false
+// === - tengmi degan buyruq bu lekin ozgaruvchini turigaham qaraydi
+// console.log(son1 != son2); // true
+// != shunchaki teng emasmi degan buyruq
+// console.log(son1 !== son2); // true
+// !== teng emasmi degan buyruq lekin bu o'zgaruvchini turigaham qaraydi
+
+
